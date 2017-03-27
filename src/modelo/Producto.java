915403c40a13686aab9298ee01feb4de92ea36af
gorka <ikaslea@1AW37>
2017-03-27 13:03:53 +0200

    ClienteModelo implementatuta

--- src/modelo/Producto.java
+++ src/modelo/Producto.java
@@ -9,4 +9,52 @@ package modelo;
  */
 public class Producto {
 	//TODO crear atributos getters setters y constructor
+int existencias;
+int id;
+String nombre;
+double precio;
+String proveedor;
+
+
+public Producto(int existencias, int id, String nombre, double precio, String proveedor) {
+	super();
+	this.existencias = existencias;
+	this.id = id;
+	this.nombre = nombre;
+	this.precio = precio;
+	this.proveedor = proveedor;
+}
+public int getExistencias() {
+	return existencias;
+}
+public void setExistencias(int existencias) {
+	this.existencias = existencias;
+}
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public double getPrecio() {
+	return precio;
+}
+public void setPrecio(double precio) {
+	this.precio = precio;
+}
+public String getProveedor() {
+	return proveedor;
+}
+public void setProveedor(String proveedor) {
+	this.proveedor = proveedor;
+}
+
+
+
 }
