14abb24df7cc79b775d1bb54938d4125a6b180a5
ikaslea <ikaslea@1aw3_07_11>
2017-03-27 13:23:40 +0200

    producto select id inplementatuta

--- src/modelo/ProductoModelo.java
+++ src/modelo/ProductoModelo.java
@@ -3,6 +3,9 @@
  */
 package modelo;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 /**
@@ -18,7 +21,22 @@ public class ProductoModelo extends Conector{
 	
 	Producto select(int id){
 		//TODO id hori duen produktua itzultzen du
-		return null;
+		try {
+			Statement st = this.conexion.createStatement();
+			ResultSet rs = st.executeQuery("select * from socios where id='" + id + "'");
+			rs.next();
+			Producto producto = new Producto();
+			producto.setId(id);
+			producto.setNombre(rs.getString("nombre"));
+			producto.setProveedor(rs.getString("proveedor"));
+			producto.setPrecio(rs.getDouble("precio"));
+			producto.setExistencias(rs.getInt("existencias"));
+			return producto;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			return null;
 	}
 	
 	public void update(Producto producto) {
